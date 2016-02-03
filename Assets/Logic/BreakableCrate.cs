1e93782e910ef786de95736047faf81d29b7ebc7
Joshua <jbhooker@mtu.edu>
2016-02-02 19:13:51 -0500

    Added a crate manager to randomly place crates with a set range of coordinates

--- Assets/Logic/BreakableCrate.cs
+++ Assets/Logic/BreakableCrate.cs
@@ -1,30 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * This class defines the behaviours of breakable crates in the game.
+ * Each crate has a set amount of durability which can only be reduced by the Player's bullets and sword currently.
+ * 
+ * @author Joshua Hooker
+ * 
+ * 1 February 2016
+ */
 public class BreakableCrate : MonoBehaviour {
 	// Prefabs associated with a crate
 	public GameObject crate;
 	public GameObject hp_bar;
-
-	private static readonly float MAX_DURABILITY = 10.0f;
-	/* The amount of damage a crate can sustain until it breaks */ 
-	private int durability;
+	// used to determine overlap amongst other Blocks
+	public bool collision_tag;
+	// initial durability of a crate
+	private static readonly float MAX_DURABILITY = 8.0f;
+	// The amount of damage a crate can sustain until it breaks
+	private float durability;
 
 	// Use this for initialization
 	void Start () {
-		durability = (int)MAX_DURABILITY;
+		durability = MAX_DURABILITY;
+		collision_tag = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		/* the HP bar shrinks as the crate's durability decreases */
 		Transform hp_len = hp_bar.transform;
-		var x_scale = ( (float)durability / MAX_DURABILITY );
+		var x_scale = ( durability / MAX_DURABILITY );
 
 		hp_len.localScale = new Vector3(x_scale, hp_len.localScale.y, hp_len.localScale.y);
 
 		/* remove the crate when its durability reaches zero */
-		if (durability <= 0) { Destroy(crate); }
+		if (durability <= 0) {
+			Destroy(crate);
+		}
 	}
 		
 	void OnTriggerEnter2D(Collider2D trigger) {
@@ -39,6 +52,12 @@ public class BreakableCrate : MonoBehaviour {
 			}
 
 			// Debug.Log("Durability: " + durability + "\n");
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D col) {
+		if (col.gameObject.tag == "Block") {
+			collision_tag = true;
 		}
 	}
 }
