e9cbd90cb3885ae64d9abb005ee4ba650d6868ba
EASEAS <EASEAS@users.noreply.github.com>
2016-02-04 18:46:27 -0500

    deleted auto generated git messages added to code

--- Assets/Logic/GenerateRoom.cs
+++ Assets/Logic/GenerateRoom.cs
@@ -1,48 +1,3 @@
-﻿using UnityEngine;
-using System.Collections;
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-public class GenerateRoom : MonoBehaviour {
-    static int roomWidth = 3;
-    static int roomHeight = 3;
-	float tileWidth = 1.5f;
-	float tileHeight = 1.5f;
-	int[,] roomMatrix;
-	GameObject blockToClone;
-
-	public GameObject[,] GeneratedRoom = new GameObject[roomWidth, roomHeight];
-    
-    // Use this for initializations
-    void Start()
-    {
-		roomMatrix = new int[,] {	{ 1,1,1 },
-									{ 0,0,0 },
-									{ 1,1,1 }
-	};
-		blockToClone = GameObject.Find("Block");
-
-		for (int i = 0; i < roomWidth; i++)
-        {
-            for (int j = 0; j < roomHeight; j++)
-            {
-				if (roomMatrix[i,j] != 0) {
-					GameObject block = (GameObject)Instantiate(blockToClone, new Vector3(j * tileHeight, i * tileWidth, 0), Quaternion.identity);
-					block.AddComponent<BoxCollider2D>();
-				}
-			}
-        }
-
-
-    }
-    // Update is called once per frame
-    void Update () {
-	
-	}
-}
-=======
-=======
->>>>>>> Fixed some github problems
 using System;
 
 public class GenerateRoom : MonoBehaviour
@@ -177,9 +132,5 @@ public class GenerateRoom : MonoBehaviour
 			}
 		}
 	}
-<<<<<<< HEAD
->>>>>>> Tiled room Gen
-=======
-}
 
->>>>>>> Fixed some github problems
+}
