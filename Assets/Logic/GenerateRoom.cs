b655ea654eba8ce8c8b290c96afc76b41d6c15b0
EASEAS <e.skogfeldt@yahoo.com>
2016-02-20 19:09:06 -0500

    Level Generation
    added:
    variable level size
    random room placement
    doughnut shaped rooms

--- Assets/Logic/GenerateRoom.cs
+++ Assets/Logic/GenerateRoom.cs
@@ -1,6 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
 using System.Collections;
-using System;
+using UnityEngine;
 
 public class GenerateRoom : MonoBehaviour
 {
@@ -11,21 +11,18 @@ public class GenerateRoom : MonoBehaviour
 	GameObject regularWall;
 	GameObject cornerWall;
 
-	public int coordinateX;
-	public int coordinateY;
 	public bool instantiate;
-	public byte door;
-	public int size;
+	public int dungeonSize;
+	int[,] floor;
+
 	// Use this for initializations
 	void Start()
 	{
 		instantiate = false;
-		door = 15;
-		size = 5;
+		dungeonSize = 10;
 		tileWidth = 1.5f;
 		tileHeight = 1.5f;
-		regularWall = GameObject.Find("Block");
-		cornerWall = GameObject.Find("Block");
+		floor = makeFloorMatrix(dungeonSize, dungeonSize, 2, 4);
 
 	}
 
@@ -34,15 +31,53 @@ public class GenerateRoom : MonoBehaviour
 	{
 		if (instantiate) // create level when instatiate is true
 		{
-			for (int i = 0; i < 12 * size; i += size + 3)
+
+			String array = "";
+			for (int i = 0; i < dungeonSize; i++)
 			{
-				for (int j = 0; j < 12 * size; j += size + 3)
+				for (int j = 0; j < dungeonSize; j++)
 				{
-					int[,] roomMatrix = makeRoomMatrix(size, size, door);
-					makeRoom(coordinateY + i, coordinateX + j, size, size, roomMatrix);
-					instantiate = false;
+					array += floor[i, j];
+					if (floor[i, j] != 0 && floor[i, j] != 2)
+					{
+						Byte doors = 0;
+						if (j - 1 >= 0)
+						{
+							if (floor[i, j - 1] != 0)
+							{
+								doors += 8;
+							}
+						}
+						if (j + 1 < dungeonSize)
+						{
+							if (floor[i, j + 1] != 0)
+							{
+								doors += 2;
+							}
+						}
+						if (i - 1 >= 0)
+						{
+							if (floor[i - 1, j] != 0)
+							{
+								doors += 4;
+							}
+						}
+						if (i + 1 < dungeonSize)
+						{
+							if (floor[i + 1, j] != 0)
+							{
+								doors += 1;
+							}
+						}
+						int[,] room = makeRoomMatrix(5, 5, doors);
+						makeRoom(i * 10, j * 10, 5, 5, room);
+					}
 				}
+				Debug.Log(array);
+				array = "";
 			}
+
+			instantiate = false;
 		}
 
 	}
@@ -84,9 +119,13 @@ public class GenerateRoom : MonoBehaviour
 		}
 		return returnMatrix;
 	}
-
+	//Places sprites into level to create a room
+	//y and x are the position of the bottom left corner of the room
+	// roomMatrix comes from the makeRoomMatrix method
 	private void makeRoom(int y, int x, int roomHeight, int roomWidth, int[,] roomMatrix)
 	{
+		regularWall = GameObject.Find("Block");
+		cornerWall = GameObject.Find("Block");
 
 		for (int i = y; i < roomWidth + y; i++)
 		{
@@ -134,5 +173,168 @@ public class GenerateRoom : MonoBehaviour
 			}
 		}
 	}
-}
 
+	int[,] makeFloorMatrix(int floorWidth, int floorHeight, int numberOfCircles, int numberOfSpecialRooms)
+	{
+		//matrix codes
+		//nothing = 0
+		//special room = 1
+		//circle rooms center = 2
+		//circle rooms edge = 3
+		//normal room = 4
+
+		int[,] returnMatrix = new int[floorWidth, floorHeight];
+		ArrayList listOfSpecialRooms = new ArrayList();
+		ArrayList listOfCircleEdgeRooms = new ArrayList();
+
+		//insert special rooms into matrix
+		//expects nothing in returnMatrix besides special rooms
+		for (int i = 0; i < numberOfSpecialRooms; i++)
+		{
+			int x = UnityEngine.Random.Range(0, floorWidth - 1);
+			int y = UnityEngine.Random.Range(0, floorHeight - 1);
+
+			//test code
+			if (returnMatrix[y, x] != 1)
+			{
+				returnMatrix[y, x] = 1;
+
+				//Add the coordinates of special rooms to connect later
+				listOfSpecialRooms.Add(y);
+				listOfSpecialRooms.Add(x);
+
+			}
+			else //if the matrix indices were already filled , retry
+			{
+				x = UnityEngine.Random.Range(0, floorWidth - 1);
+				y = UnityEngine.Random.Range(0, floorHeight - 1);
+				i--;
+			}
+
+		}
+
+		//insert circle centers
+		for (int i = 0; i < numberOfCircles; i++)
+		{
+			int x = UnityEngine.Random.Range(0, floorWidth - 1);
+			int y = UnityEngine.Random.Range(0, floorHeight - 1);
+			if (returnMatrix[y, x] != 1 && returnMatrix[y, x] != 2)
+			{
+				returnMatrix[y, x] = 2;
+			}
+			else //if the matrix indices were already filled , retry
+			{
+				x = UnityEngine.Random.Range(0, floorWidth - 1);
+				y = UnityEngine.Random.Range(0, floorHeight - 1);
+				i--;
+			}
+
+		}
+
+		//expande circle centers into circles
+		for (int i = 0; i < floorWidth; i++)
+		{
+
+			for (int j = 0; j < floorHeight; j++)
+			{
+
+				if (returnMatrix[j, i] == 2)
+				{
+
+					//create rooms around center
+					for (int x = i - 1; x < i + 2; x++)
+					{
+						for (int y = j - 1; y < j + 2; y++)
+						{
+
+
+							//check to make sure circle-edge room being added is in domain and range of floorMatrix
+							if ((x >= 0 && y >= 0) && (x < floorWidth && y < floorHeight))
+							{
+
+								//check if room is already made there
+								if (returnMatrix[y, x] == 0)
+								{
+									returnMatrix[y, x] = 3;
+
+									//Add coordinates for pathing to special rooms later
+									listOfCircleEdgeRooms.Add(x);
+									listOfCircleEdgeRooms.Add(y);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		//start pathing for each of the special rooms
+		//defines position as index in numberOfSpecialRooms
+		for (int i = 0; i < numberOfSpecialRooms; i++)
+		{
+			//pick a circle-edge to aim towards 
+			//defines position as index in listOfCircleEdgeRooms
+
+			int circleEdgeTarget = UnityEngine.Random.Range(0, listOfCircleEdgeRooms.Count / 2);
+			bool foundPath = false;
+			int x = (int)listOfSpecialRooms[2 * i + 1];
+			int y = (int)listOfSpecialRooms[2 * i];
+
+			//start building x towards a circle
+			for (; x != (int)listOfCircleEdgeRooms[circleEdgeTarget * 2];)
+			{
+
+				if (x < (int)listOfCircleEdgeRooms[circleEdgeTarget * 2])
+				{
+					x++;
+				}
+				else
+				{
+					x--;
+				}
+
+				//if room hasnt been pathed to closer room 
+				if (returnMatrix[y, x] == 0)
+				{
+					//adds new normal room into to create path
+					returnMatrix[y, x] = 4;
+				}
+				else
+				{
+
+					foundPath = true;
+					break;
+				}
+			}
+
+			if (!foundPath)
+			{
+				for (; y != (int)listOfCircleEdgeRooms[circleEdgeTarget * 2 + 1];)
+				{
+					if (y < (int)listOfCircleEdgeRooms[circleEdgeTarget * 2 + 1])
+					{
+						y++;
+					}
+					else
+					{
+						y--;
+					}
+
+					if (returnMatrix[y, x] == 0)
+					{
+						//adds new normal room into to create path
+						returnMatrix[y, x] = 4;
+					}
+					else
+					{
+						foundPath = true;
+					}
+				}
+			}
+
+		}
+
+		return returnMatrix;
+	}
+
+}
