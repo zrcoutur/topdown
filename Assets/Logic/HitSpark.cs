e6d7e863311214be2d99a8e16659c268c6ab8544
zrcoutur <zrcoutur@mtu.edu>
2016-03-03 16:28:40 -0500

    Added Numerous Things
    Many things: player death, damagae, scrap, energy cores, enemy death,
    several effects, several system reworks, the list goes on...

---
+++ Assets/Logic/HitSpark.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitSpark : Animatable {
+
+	// Use this for initialization
+	void Start () {
+
+		base.timer = 0.3f;
+
+	}
+
+}
---
+++ Assets/Shine.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shine : Animatable {
+
+	// Use this for initialization
+	void Start () {
+
+		base.timer = 0.3f;
+
+	}
+
+}
