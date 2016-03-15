d5a3469f776ba1fb120bfe477460051b80ed6d3d
Joshua <jbhooker@mtu.edu>
2016-03-15 13:44:07 -0400

    Modified item drop despawn and fixed enemy spawn rates.

--- Assets/Logic/Item_Drop.cs
+++ Assets/Logic/Item_Drop.cs
@@ -2,18 +2,23 @@
 using UnityEngine;
 
 public class Item_Drop : MonoBehaviour {
+
+	public Shine shine;
 	/* How long the item will stay in the game before disappearing */
 	private float duration;
 
 	public void Start() {
 		// Initialize timer
-		duration = 4.5f;
+		duration = 7.5f * (UnityEngine.Random.Range(50, 150) / 100f);
 	}
 
 	public void Update() {
 		if (duration >= 0) { // Decrement timer
 			duration -= Time.deltaTime;
 		} else { // Remove item from the game
+			// Shine effect
+			Instantiate(shine, gameObject.transform.position, Quaternion.Euler (0, 0, 0));
+
 			Destroy(this.gameObject);
 		}
 	}
